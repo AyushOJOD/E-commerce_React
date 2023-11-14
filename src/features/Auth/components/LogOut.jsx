1a24582ef1f99c39f892fd1cbafecc64a56a790e
Ayush <rishiustaad@gmail.com>
2023-11-14 15:55:30 +0530

    Minor bugs

--- src/features/Auth/components/LogOut.jsx
+++ src/features/Auth/components/LogOut.jsx
@@ -3,22 +3,17 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { selectAuthState, selectLoggedInUser, signOutAsync } from '../authSlice';
 import { Navigate } from 'react-router-dom';
-import Loader from '../../common/Loader';
 
 const LogOut = () => {
     const dispatch = useDispatch();
     const user = useSelector(selectLoggedInUser);
-    const status = useSelector(selectAuthState);
 
     useEffect(() => {
         dispatch(signOutAsync());
     });
 
     return (
-        <div>
-            {status === 'loading' && <Loader />}
-            {status === 'idle' && !user && <Navigate to='/login' replace={true} />}
-        </div>
+        <>{!user && <Navigate to="/login" replace={true}></Navigate>}</>
     )
 }
 
